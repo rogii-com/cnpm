32afcb27f19f09a7aee9e528ac788e3e6a5ae5e8
Georgy Shepelev <geo.shepelev@gmail.com>
2020-06-23 08:26:40 +0400

    issue rogii-com/cnpm#13
    add show_package_component script and cnpm_add_package2 implementation

---
+++ CNPM_ADD_PACKAGE2.cmake
@@ -0,0 +1,44 @@
+function(
+    NPM_ADD_PACKAGE
+)
+    set(
+        oneValueArgs
+        NAME VERSION ARCHITECTURE BUILD_NUMBER TAG
+    )
+
+    cmake_parse_arguments(
+        NPM_ARGS
+        ""
+        "${oneValueArgs}"
+        ""
+        ${ARGN}
+    )
+
+    string(
+        TOUPPER
+            "${SHOW_ARGUMENT}"
+            SHOW_ARGUMENT
+    )
+
+    foreach(argument ${oneValueArgs})
+        if(argument STREQUAL SHOW_ARGUMENT)
+            execute_process(
+                COMMAND
+                    ${CMAKE_COMMAND}
+                    -E
+                    echo_append
+                    "${NPM_ARGS_${SHOW_ARGUMENT}}"
+                WORKING_DIRECTORY
+                    ${CMAKE_CURRENT_LIST_DIR}
+            )
+
+            return()
+        endif()
+    endforeach()
+
+    message(
+        FATAL_ERROR
+        "Unknown argument name: '${SHOW_ARGUMENT}'"
+    )
+endfunction()
+
